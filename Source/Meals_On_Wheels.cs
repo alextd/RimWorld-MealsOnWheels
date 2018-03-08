9ed0f6072e6c36ab010ea773b6068eaca4e502fb
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-03-08 11:59:08 -0800

    Commenting out unused settings

--- Source/Meals_On_Wheels.cs
+++ Source/Meals_On_Wheels.cs
@@ -10,7 +10,7 @@ namespace Meals_On_Wheels
 		public Mod(ModContentPack content) : base(content)
 		{
 			// initialize settings
-			GetSettings<Settings>();
+			//GetSettings<Settings>();
 #if DEBUG
 			HarmonyInstance.DEBUG = true;
 #endif
@@ -18,15 +18,15 @@ namespace Meals_On_Wheels
 			harmony.PatchAll(Assembly.GetExecutingAssembly());
 		}
 
-		public override void DoSettingsWindowContents(Rect inRect)
-		{
-			base.DoSettingsWindowContents(inRect);
-			GetSettings<Settings>().DoWindowContents(inRect);
-		}
+		//public override void DoSettingsWindowContents(Rect inRect)
+		//{
+		//	base.DoSettingsWindowContents(inRect);
+		//	GetSettings<Settings>().DoWindowContents(inRect);
+		//}
 
-		public override string SettingsCategory()
-		{
-			return "Meals On Wheels";
-		}
+		//public override string SettingsCategory()
+		//{
+		//	return "Meals On Wheels";
+		//}
 	}
 }
